2ccfed460b326510ffb3a2e81f588211fadc9174
Enzo Rafael <enzoricarteaa@gmail.com>
2025-03-15 12:40:18 -0300

    Update POneImputs.cs

--- Unity/Candle_Light/Assets/Scriptis/ImputSystem/POneImputs.cs
+++ Unity/Candle_Light/Assets/Scriptis/ImputSystem/POneImputs.cs
@@ -15,15 +15,23 @@ public class POneImputs : MonoBehaviour
         _Input = new PlayersImputMap();
         _Input.Player1Muve.Enable();
         _Input.Player1Muve.MuveImput.performed += SetMuve;
+        _Input.Player1Muve.ActionInput.performed += SetBtn;
         _Input.Player1Muve.MuveImput.canceled += SetMuve;
+        _Input.Player1Muve.ActionInput.canceled += SetBtn;
+        
     }
     private void OnDisable()//Desativa os imputs
     {
         _Input.Player1Muve.MuveImput.performed -= SetMuve;
         _Input.Player1Muve.MuveImput.canceled -= SetMuve;
+        _Input.Player1Muve.ActionInput.performed -= SetBtn;
+        _Input.Player1Muve.ActionInput.canceled -= SetBtn;
         _Input.Player1Muve.Disable();
     }
     void SetMuve(InputAction.CallbackContext ctx){
         MoveImput = ctx.ReadValue<Vector3>();
+    }
+    void SetBtn(InputAction.CallbackContext ctx){
+        ActionImput = ctx.ReadValue<bool>();
     }
 }

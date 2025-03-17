e94c799825f78dbdc886ce76de031835342dcf66
Alu <vitinh.do.cs@gmail.com>
2025-03-17 15:00:49 -0300

    Branch detecçao de luz
    Commit inicial, nao cheguei a implementar nada ainda

---
+++ Unity/Candle_Light/Assets/Scripts/LightSystems/LightSystem.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel;
+using UnityEngine;
+/// <summary>
+/// Sistema que roda os calculos de deteccao de luz
+/// </summary>
+[AddComponentMenu(menuName:"/")]
+public class LightSystem : Singleton<LightSystem>
+{
+    private static 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity/Candle_Light/Assets/Scripts/Util/Singleton.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+/// <summary>
+/// Singleton de tipagem generica.
+/// <para/>Garante que havera apenas uma instancia da classe em cena.&#xD;&#10;&#xA;
+/// <para/>Para aplicar o design pattern, herde a classe de Singleton como o exemplo: 
+/// <para/><![CDATA[public class Exemplo : Singleton&LT;Exemplo&GT;]]>
+/// </summary>
+public class Singleton<T> : MonoBehaviour where T: Singleton<T>
+{
+    /// <summary>
+    /// Instancia unica da classe com padrao singleton
+    /// </summary>
+    public static T Instance
+    {
+        get
+        {
+            // Se a instancia nao existir, cria uma e atribui
+            if(instance == null) instance = new GameObject().AddComponent<T>();
+
+            return instance;
+        }
+    }
+    private static T instance;
+
+    void Reset()
+    {
+        Debug.LogError("=== Componente singleton nao deve ser adicionado pelo inspetor ===");
+        //gostaria de destruir automaticamente aqui mas a unity nao deixa
+        //DestroyImmediate(this);
+    }
+}

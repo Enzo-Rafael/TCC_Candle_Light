389fa0afd86989f20c2c1cc97218559d81e7524d
Alu <vitinh.do.cs@gmail.com>
2025-03-26 15:25:03 -0300

    Animações
    -Andar
    -Idle
    -Tique Idle

--- Unity/Candle_Light/Assets/Scripts/PlayersScripts/PlayerOneScript.cs
+++ Unity/Candle_Light/Assets/Scripts/PlayersScripts/PlayerOneScript.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Animations;
 using UnityEngine;
 
 //Player 1: Move WASD
@@ -10,6 +11,7 @@ public class PlayerOneScript : MonoBehaviour
     Vector3 playerMove;
     public float velocity;
     public CharacterController controller;
+    public Animator animator;
     private bool groundedPlayer;
     private float gravityValue = -9.81f;
     public GameObject mainCam;// referencia para a o andar do player a partir da camera
@@ -45,6 +47,7 @@ public class PlayerOneScript : MonoBehaviour
         {
             gameObject.transform.forward = playerMove;
         }
+        animator.SetFloat("WalkSpeed", playerMove.magnitude);
         playerMove.y += gravityValue * Time.deltaTime;//Gravidade do player 1
         controller.Move(playerMove);
     }

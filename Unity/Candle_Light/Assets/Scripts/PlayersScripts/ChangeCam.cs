40db91d28dd3b7fe3fca0d48b4373dc1bf4d93b5
Enzo Rafael <enzoricarteaa@gmail.com>
2025-03-25 21:54:52 -0300

    inicial

---
+++ Unity/Candle_Light/Assets/Scripts/PlayersScripts/ChangeCam.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+//using Cinemachine;
+using System;
+
+public class ChangeCam : MonoBehaviour
+{
+   /* public CinemachineVirtualCamera[] camRef;
+    private CinemachineVirtualCamera currentCam;
+
+    void Start(){
+        currentCam = camRef[0];
+        foreach(CinemachineVirtualCamera ref cam in camRef){
+            if(camRef == currentCam){
+                cam.Priority = 1;
+            }else{
+                cam.Priority = 0;
+            }
+        }
+    }
+    public void ChangeCam(){
+
+    }*/
+}

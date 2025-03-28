e69d74b7107cdfff478f237c2889dfe0b47d955e
gs144 <otaviobiavateoliveira@hotmail.com>
2025-03-27 22:46:53 -0300

    Main Menu functional

--- Unity/Candle_Light/Assets/Scripts/UI/UIMainMenu.cs
+++ Unity/Candle_Light/Assets/Scripts/UI/UIMainMenu.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIMainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject settingsPanel;
+    public GameObject creditsPanel;
+    public GameObject exitPanel;
+
+    public void LoadScene()
     {
-        
+        SceneManager.LoadScene("SampleScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenSettings()
     {
-        
+        settingsPanel.SetActive(true);
+    }
+
+    public void OpenCredits()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    public void ReturnCredits()
+    {
+        creditsPanel.SetActive(false);
+    }
+
+    public void OpenExit()
+    {
+        exitPanel.SetActive(true);
+    }
+
+    public void ReturnExit()
+    {
+        exitPanel.SetActive(false);
     }
 }

2e1886ac63bd64e67722cf0eea571ed837ac1501
Alu <vitinh.do.cs@gmail.com>
2025-03-28 17:26:51 -0300

    Efeito Escuridão V2
    • Troca do efeito procedural de espinho por espinhos mais complexos, usando textura.
    • Implementação da mudança de intensidade do efeito, de acordo com tempo no escuro.

---
+++ Unity/Candle_Light/Assets/Scripts/PlayersScripts/DarknessTimer.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+/// <summary>
+/// Comunica o efeito de consumo da escuridao com o fantasma e UI
+/// </summary>
+[RequireComponent(typeof(LightDetector))]
+public class DarknessTimer : MonoBehaviour
+{
+    private LightDetector detector;
+    
+    [Tooltip("Volume contendo o componente DarknessEffect para a tela correta.")]
+    [SerializeField] private Volume postProcessingVolume;
+
+    private DarknessEffectVolumeComponent darknessEffect;
+
+    [Tooltip("Tempo que leva para o objeto ir de iluminado para completamente tomado por trevas.")]
+    [SerializeField] private float darkTime;
+
+    [Tooltip("Tempo que leva para o objeto ir de completamente tomado para iluminado")]
+    [SerializeField] private float lightTime;
+
+    private float timer; 
+
+    void Awake()
+    {
+        detector = GetComponent<LightDetector>();
+        postProcessingVolume.profile.TryGet(out darknessEffect);
+        timer = 0;
+    }
+
+    void FixedUpdate()
+    {
+        if(detector.IsLit)
+        {
+            timer = Mathf.Max(timer - (1/lightTime) * Time.fixedDeltaTime, 0);
+        }else
+        {
+            timer = Mathf.Min(timer + (1/darkTime) * Time.fixedDeltaTime, 1);
+            if(timer >= 1)
+            {
+                Debug.Log("Escuridao no maximo, fantasma morreria aqui.");
+            }
+        }
+
+        darknessEffect.intensity.value = timer;
+    }
+}

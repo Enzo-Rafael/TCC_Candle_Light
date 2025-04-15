703fb64babba3b22f5f39d197b87b18910623354
Enzo Rafael <enzoricarteaa@gmail.com>
2025-04-14 21:11:18 -0300

    retirada da trava de jogodor

--- Unity/Candle_Light_clone_0/Assets/Scripts/OnlineMultplayer/CreateSelection.cs
+++ Unity/Candle_Light_clone_0/Assets/Scripts/OnlineMultplayer/CreateSelection.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using Mirror;
 using Unity.Cinemachine;
 using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class CreateSelection : NetworkBehaviour
     {
@@ -18,9 +19,9 @@ public class CreateSelection : NetworkBehaviour
         [SerializeField] private float turnSpeed = 90f;//Velocidade da troca de seleção de personagens
         [SerializeField] private Character[] characters = default;//Lista ScriptableObjects dos personageens
         [NonSerialized]public bool P1Selected = false , P2Selected = false;//checar se o personagem foi escolhiido
-        [SerializeField] private GameObject btnPlay;//para lemitar a escolha de personagens
         private int currentCharacterIndex = 0;//Index dos persongens
         private List<GameObject> characterInstances = new List<GameObject>();//Lista da preview dos personagens
+        
 
         private CinemachineInputAxisController cam2;
 
@@ -50,35 +51,33 @@ public class CreateSelection : NetworkBehaviour
 
     private void Update()
     {
-     characterPreviewParent.RotateAround(characterPreviewParent.position,characterPreviewParent.up,turnSpeed * Time.deltaTime);    
+     characterPreviewParent.RotateAround(characterPreviewParent.position,characterPreviewParent.up,turnSpeed * Time.deltaTime);
+       
     }
 
     public void Select(){//Confirma a opção excolhida de qual persongem vai ser jogado
-        PlayerOneScript P1 = FindAnyObjectByType<PlayerOneScript>();
-        PlayerTwoScript P2 = FindAnyObjectByType<PlayerTwoScript>();
-        if(P1 != null)P1Selected = false;
-        if(P2 != null)P2Selected = false;
+        /*if((currentCharacterIndex == 0 && P1Selected == false) || (currentCharacterIndex == 1 && P2Selected == false)){//Trava de um personagem por jogador
+            CmdSelect(currentCharacterIndex);
+            characterSelectDisplay.SetActive(false); 
+        }else{
+            BtnChangeLeft();
+        }*/
         CmdSelect(currentCharacterIndex);
-        characterSelectDisplay.SetActive(false);
+        characterSelectDisplay.SetActive(false); 
+        
     }
     [Command(requiresAuthority = false)]
-    public void CmdSelect(int characterIndex, NetworkConnectionToClient sender = null){
-        if(characterIndex == 0 && P1Selected == false || characterIndex == 1 && P2Selected == false){//O jogo nescessita dos dois persongens para funcionar 
-            if(characterIndex == 0)P1Selected = true;
-            if(characterIndex == 1){
-                P2Selected = true;
-            }   
-        }else if(characterIndex == 0 && P1Selected == true){
-            characterIndex = 1; 
-        }else if(characterIndex == 1 && P2Selected == true){
-            characterIndex = 0;
+    public void CmdSelect(int characterIndex, NetworkConnectionToClient sender = null){//O jogo nescessita dos dois persongens para funcionar 
+        if(characterIndex == 1){
+            cam2.enabled = true;
+            P2Selected = true;
+        } else{
+            cam2.enabled = false;
+            P1Selected = true;
         }
-        if(characterIndex == 0) cam2.enabled = false;
         GameObject characterInstance = Instantiate(characters[characterIndex].GameplayCharacterPrefab);
         
-        NetworkServer.Spawn(characterInstance, sender);  
-        
-        
+        NetworkServer.Spawn(characterInstance, sender);
     }
 
     public void BtnChangeRight(){//Vai fazer a troca de personagem levando o a auteração de valores para a direita
@@ -101,5 +100,6 @@ public class CreateSelection : NetworkBehaviour
         characterInstances[currentCharacterIndex].SetActive(true);
         characterNameText.text = characters[currentCharacterIndex].CharacterName;
     }
+
 }
 

9a6b16b70928cfbf5c8af444ba7adbc812ec2bb2
Enzo Rafael <enzoricarteaa@gmail.com>
2025-05-16 21:41:25 -0300

    FIX

--- Unity/Candle_Light/Assets/Scripts/OnlineMultplayer/DisableMenu.cs
+++ Unity/Candle_Light/Assets/Scripts/OnlineMultplayer/DisableMenu.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class DisableMenu : MonoBehaviour
 {
+     private void Start()
+    {
+         Cursor.lockState = CursorLockMode.None;
+         Cursor.visible = true; 
+
+    }
+       
     private void OnDisable()
     {
-         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 }

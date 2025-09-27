8827aa49592d0eee2fdeaa820c9deac01f82c6c0
Guilherme D. <guilherme.dias69420@gmail.com>
2025-09-27 11:13:47 -0300

    Olhinho seguindo Player 1
    Script bem rudimentar (não tem rotação máxima nem nada) mas acho que vai ser bom pro playtest. Fiz isso segunda passada e to só comitando agr

---
+++ Unity/Candle_Light/Assets/Scripts/LightSystem/EyeTargetPlayer.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class EyeTargetPlayer : MonoBehaviour
+{
+    LightDetector lightDetector;
+    public Transform innerEye;
+    public Transform outerEye;
+    private Transform playerOne;
+
+    void Awake()
+    {
+        lightDetector = GetComponent<LightDetector>();
+        //Me desculpa, deve ter alguma maneira muito programador de fazer isso bem melhor
+        playerOne = FindFirstObjectByType<PlayerOneScript>().transform;
+    }
+
+    
+    void FixedUpdate()
+    {
+        if (playerOne != null && lightDetector.IsLit == false)
+        {
+
+            innerEye.LookAt(new Vector3(playerOne.position.x, playerOne.position.y+2, playerOne.position.z), Vector3.up);
+            outerEye.LookAt(new Vector3(playerOne.position.x/1, playerOne.position.y+2, playerOne.position.z/1), Vector3.up);
+            //outerEye.LookAt(new Vector3(playerOne.position.x/2, playerOne.position.y/2, playerOne.position.z/2));
+        }
+        else
+        {
+            innerEye.rotation = Quaternion.identity;
+            outerEye.rotation = Quaternion.identity;
+        }
+    }
+}

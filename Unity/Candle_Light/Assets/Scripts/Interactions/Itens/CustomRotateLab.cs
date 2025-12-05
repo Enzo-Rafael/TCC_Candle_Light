24861efa201f6a7b377be7fe19704b022af95386
Ita-lack <166967267+Ita-lack@users.noreply.github.com>
2025-12-05 11:38:50 -0300

    olinhos de baixo salvando

--- Unity/Candle_Light/Assets/Scripts/Interactions/Itens/CustomRotateLab.cs
+++ Unity/Candle_Light/Assets/Scripts/Interactions/Itens/CustomRotateLab.cs
@@ -1,49 +1,50 @@
 using UnityEngine;
 using System.Collections;
-using System;
 
 public class CustomRotateLab : MonoBehaviour, ICodeCustom
 {
-    [SerializeField]
-    int rotState;
+    [Header("Settings")]
+    [SerializeField] private float rotationDuration = 0.5f; 
+    [SerializeField] private bool clockwise = true; 
+    [Header("Debug Info")]
+    [SerializeField] int rotState; 
+    [Header("Axes (Select only one)")]
+    [SerializeField] [Range(0, 1)] private int axisX;
+    [SerializeField] [Range(0, 1)] private int axisY;
+    [SerializeField] [Range(0, 1)] private int axisZ;
+    [SerializeField] private bool isInitialized = false; 
 
-    [SerializeField]
-    private bool isRotating;
-    [SerializeField]
-    [Range(0, 1)]
-    private int axisX;
-
-    [SerializeField]
-    [Range(0, 1)]
-    private int axisY;
-
-    [SerializeField]
-    [Range(0, 1)]
-    private int axisZ;
-    Vector3 startEuler;
-    private bool isPlayerPresent = false;
-    private void Start()
-    {
-        startEuler = transform.localEulerAngles;
-        if (axisX == 1) { rotState = (4 - (int)(Math.Abs(startEuler.x) / 90)) % 4; }
-        else if (axisY == 1) { rotState = (4 - (int)(Math.Abs(startEuler.y) / 90)) % 4; }
-        else if (axisZ == 1) { rotState = (4 - (int)(Math.Abs(startEuler.z) / 90)) % 4; }
+    private void UpdateStateFromRotation(){
+        float currentAngle = 0f;
+        if (axisX == 1) currentAngle = transform.localEulerAngles.x;
+        else if (axisY == 1) currentAngle = transform.localEulerAngles.y;
+        else if (axisZ == 1) currentAngle = transform.localEulerAngles.z;
+        int angleInt = Mathf.RoundToInt(currentAngle);
+        rotState = Mathf.Abs(angleInt / 90) % 4;
     }
 
     public void CustomBaseAction(object additionalInformation){
+        if (!isInitialized){
+            UpdateStateFromRotation();
+            isInitialized = true;
+        }
         StartCoroutine(RotCoroutine());
     }
     private IEnumerator RotCoroutine(){
-        rotState = (rotState + 1) % 4;
-        if (axisX == 1) startEuler.x = startEuler.x ==  360 ? 0 : +90 * rotState;
-        if (axisY == 1) startEuler.y = startEuler.y ==  360 ? 0 : +90 * rotState;
-        if (axisZ == 1) startEuler.z = startEuler.z == -360 ? 0 : -90 * rotState;
-        Quaternion targetRot = Quaternion.Euler(startEuler);
-        while (Quaternion.Angle(transform.localRotation, targetRot) > 1f)
+        Vector3 rotateAxis = new Vector3(axisX, axisY, axisZ);
+        float angleStep = clockwise ? 90f : -90f;
+
+        Quaternion startRot = transform.localRotation;
+        Quaternion targetRot = startRot * Quaternion.Euler(rotateAxis * angleStep);
+
+        float elapsedTime = 0f;
+        while (elapsedTime < rotationDuration)
         {
-            transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRot, 0.1f);
-            yield return new WaitForFixedUpdate();
+            transform.localRotation = Quaternion.Slerp(startRot, targetRot, elapsedTime / rotationDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null; 
         }
         transform.localRotation = targetRot;
+        rotState = (rotState + 1) % 4;   
     }
 }

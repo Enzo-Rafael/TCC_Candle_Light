52f0e00289e3eb4a86d730962b4ae1f5e5157fbb
Alu <vitinh.do.cs@gmail.com>
2025-12-06 20:13:32 -0300

    Grade se some quando ta abaixada

---
+++ Unity/Candle_Light/Assets/Scripts/StateMachineBehaviours/HideWhileInState.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class HideWhileInState : StateMachineBehaviour
+{
+    Vector3 origScale;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        origScale = animator.transform.localScale;
+    }
+
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(!animator.IsInTransition(0))
+        {
+            animator.transform.localScale = Vector3.zero;
+        }
+        else
+        {
+            animator.transform.localScale = origScale;
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}

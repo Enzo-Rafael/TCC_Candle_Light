df866c35c5a17e990be3abec3a7414dc22cd7b94
Denys Grossi <denysgabrielg.pereira@gmail.com>
2025-12-07 19:21:25 -0300

    Ending Fix
    Features Fix

--- Unity/Candle_Light/Assets/Scripts/Util/LoadOnVideoEnd.cs
+++ Unity/Candle_Light/Assets/Scripts/Util/LoadOnVideoEnd.cs
@@ -7,10 +7,14 @@ public class LoadOnVideoEnd : MonoBehaviour
 
     [SerializeField] private string sceneName;
     private VideoPlayer vp;
+    public bool goToEnd;
 
     void Start()
     {
-        UISplashControl.Instance.SetState(true);
+        if (goToEnd)
+        {
+            UISplashControl.Instance.SetState(true);
+        }
         vp = GetComponent<VideoPlayer>();
         vp.SetDirectAudioVolume(0, AudioManager.Instance.musicVolume*AudioManager.Instance.masterVolume);
         vp.loopPointReached += (vp) => { SceneManager.LoadScene(sceneName); };
